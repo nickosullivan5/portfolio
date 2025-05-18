743ffc06314c84bdaf7a6c5e476365fc8a9560a2
nos <osullivan.nick@icloud.com>
2025-05-18 15:03:46 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./sections/Navbar.jsx";
+
+function App() {
+  return (
+      <main className="max-w-6xl mx-auto">
+        <Navbar className="z-50"/>
+
+
+
+      </main>
+
+
+  )
+}
+
+export default App
---
+++ src/sections/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <header className="fixed top-0 left-0 right-0 z-50">
+            <div className ="max-w-6xl mx-auto">
+                <div className="text-black flex justify-between items-center py-5 mx-auto">
+                    <a href="/" className=" text-4xl text-black font-bold hover:text-white transition-colors">
+                            nos portfolio
+                    </a>
+                    <b>
+                    left_brain
+                    right_brain
+                    </b>
+                </div>
+            </div>
+            <hr className="mt-2 mx-auto pt-0" />
+
+        </header>
+    )
+}
+export default Navbar
